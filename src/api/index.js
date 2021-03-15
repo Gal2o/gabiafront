51b3934ea3f9254b58643ebc3e74859f9182ea88
didrlgus <rlgusdid@naver.com>
2021-03-15 19:54:55 +0900

    401 access deny response 처리 로직 공통화

--- src/api/index.js
+++ src/api/index.js
@@ -1,36 +1,10 @@
-import axios from 'axios'
 import router from '../router'
 
-const DOMAIN = 'http://139.150.76.156:8001'
 const UNAUTHORIZED = 401
+
 const onUnauthorized = () => {
+  alert('로그인이 필요한 서비스 입니다.')
   router.push('/login')
 }
 
-const request = (method, url, data) => {
-  return axios({
-    method,
-    url: DOMAIN + url,
-    data
-  }).then(result => result.data)
-    .catch(result => {
-      const {status} = result.response
-      if (status === UNAUTHORIZED) {
-        return onUnauthorized()
-      }
-
-      throw Error(result)
-    })
-}
-
-export const book = {
-  fetch() {
-    return request('get', '/book-service/books')
-  },
-  fetchWithPage(page) {
-    return request('get', '/book-service/books?page=' + page)
-  },
-  fetchDetails(id) {
-    return request('get', '/book-service/books/' + id)
-  }
-}
+export { UNAUTHORIZED, onUnauthorized }
